012cd96c064def138e4faae4363ceed580fd5499
youssefnasser5446-crypto <youssef.nasser5446@gmail.com>
2026-08-13 01:31:33 +0300

    Question01

--- AssignmentSession3OOP/Program.cs
+++ AssignmentSession3OOP/Program.cs
@@ -4,7 +4,24 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Question01
+
+            /*
+              a)  What is the difference between Method Overloading and Method Overriding?
+
+            ans => 1] Method overloading occurs at compile time, whereas method overriding occurs at runtime.
+                   2] Method overloading same name but different parameter list, but overriding must same signature.
+
+            b)  What is the difference between Static Binding and Dynamic Binding?
+
+                 ans => Static binding is fast because it is executed at compile-time; 
+            the reference determines which method is called. In contrast,
+            dynamic binding is slower than static binding because the call occurs at runtime,
+            and the object determines which method is called.  
+
+             */
+
+            #endregion
         }
     }
 }
